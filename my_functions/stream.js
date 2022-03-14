ff8f4ad78f251ce8c7a01a155921b60c411026a5
AkashDeep000 <akashdeepdas970@gmail.com>
2022-03-15 01:35:03 +0530

    wangler

--- my_functions/stream.js
+++ my_functions/stream.js
@@ -1,36 +1,43 @@
 import ytdl from "ytdl-core"
 import fs from "fs"
 exports.handler = async function (event, context) {
-  const id = event.queryStringParameters.id
-  /* let stream = await ytdl(`https://www.youtube.com/watch?v=${id}`, {
-    filter: "audioonly",
-    dlChunkSize: 0,
-  })
-  .on('end', () => {
-    console.log('Successfully downloaded the stream!');
-  });
-*/
+  const id = event.queryStringParameters.id;
+  const format = event.queryStringParameters.format;
+  // const quality = event.queryStringParameters.quality;
   const url = `https://www.youtube.com/watch?v=${id}`;
-
+  
+  let resFormat = "mp3";
+  let extention = "mp3";
+  if (format === "m4a" || "m4r") {
+    resFormat = 'm4a';
+    extention = format;
+  } else if (format === "mp3") {
+    resFormat = 'mp3';
+      extention = format;
+  }
+  
+console.log(extention)
   const requestYTDLStream = (url) => new Promise(async (resolve, reject) => {
     const stream = ytdl(url, {
-      //  filter: 'audioonly',
-      quality: "lowestaudio",
-
-      dlChunkSize: 0,
+      //filter: format => format.container === container,
+      quality: 'lowestaudio',
+      // dlChunkSize: 0,
+      filter: 'audioonly',
+      format: resFormat,
     });
-    stream.pipe(await fs.createWriteStream(`/tmp/audio.mp3`))
+    stream.pipe(await fs.createWriteStream(`./audio.${extention}`))
     stream.on("finish", () => resolve(stream)).on("error", err => reject(err));
   });
   await requestYTDLStream(url)
-  const stream = fs.readFileSync(`/tmp/audio.mp3`)
+  const stream = fs.readFileSync(`./audio.${extention}`)
   console.log(stream)
 
   return {
     statusCode: 200,
     headers: {
-      "Content-Type": "audio/mp3",
-      "Cache-Control": "s-maxage=2592000, max-age=86400",
+      "Content-Type": `audio/${extention}`,
+      "Content-Disposition": `attachment; filename="audio.${extention}"`,
+     "Cache-Control": "s-maxage=2592000, max-age=86400",
     },
     body: stream.toString("base64"),
     isBase64Encoded: true,
